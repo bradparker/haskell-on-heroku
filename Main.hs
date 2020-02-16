a04ad0c65855f66612695e5916da20b75fddbc2a
Brad Parker <hi@bradparker.com>
2020-02-16 12:56:13 +1000

    Add a pretty cool Haskell web app

--- Main.hs
+++ Main.hs
@@ -1,4 +1,32 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wall #-}
+
 module Main where
 
+import Control.Applicative ((<|>))
+import Data.Proxy (Proxy (Proxy))
+import Network.Wai.Handler.Warp (run)
+import Servant
+  ( (:>),
+    Capture,
+    Get,
+    Handler,
+    PlainText,
+    serve,
+  )
+import System.Environment (getEnv)
+
+type Greeter =
+  "hello"
+    :> Capture "name" String
+    :> Get '[PlainText] String
+
+greet :: String -> Handler String
+greet name = pure $ "Hello, " <> name <> "!"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  port <- read <$> getEnv "PORT" <|> pure 8000
+  run port $ serve (Proxy @Greeter) greet
